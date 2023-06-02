6af9e329d7820cba1adf8f6886752d8fd46e6ae6
Aufa Rizky <aufarizky202@gmail.com>
2023-06-02 13:59:35 +0700

    Add Login API in ApiService

--- app/src/main/java/com/example/loginregistermockup/api/ApiService.kt
+++ app/src/main/java/com/example/loginregistermockup/api/ApiService.kt
@@ -1,4 +1,12 @@
 package com.example.loginregistermockup.api
 
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface ApiService {
+    @FormUrlEncoded
+    @POST("login")
+    fun loginApi(@Field("username") username: String, @Field("password") password: String) : Call<LoginResponse>
 }
