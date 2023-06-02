877a975a91d432b4d375cc5a0b5867f3f8e6259b
Aufa Rizky <aufarizky202@gmail.com>
2023-06-02 14:00:43 +0700

    Add RegisterResponse

---
+++ app/src/main/java/com/example/loginregistermockup/api/RegisterResponse.kt
@@ -0,0 +1,15 @@
+package com.example.loginregistermockup.api
+
+import com.google.gson.annotations.SerializedName
+
+data class RegisterResponse(
+
+	@field:SerializedName("code")
+	val code: Int,
+
+	@field:SerializedName("message")
+	val message: String,
+
+	@field:SerializedName("status")
+	val status: String
+)
