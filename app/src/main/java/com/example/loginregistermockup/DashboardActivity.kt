01c147341565c45d1743e1c9c0e197e7e6eea9ba
Aufa Rizky <aufarizky202@gmail.com>
2023-06-06 09:42:00 +0700

    Implement Logout feature in DashboardActivity

--- app/src/main/java/com/example/loginregistermockup/DashboardActivity.kt
+++ app/src/main/java/com/example/loginregistermockup/DashboardActivity.kt
@@ -1,9 +1,18 @@
 package com.example.loginregistermockup
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
 import com.example.loginregistermockup.databinding.ActivityDashboardBinding
+import com.example.loginregistermockup.token.TokenPreference
+import com.example.loginregistermockup.token.TokenViewModel
+import com.example.loginregistermockup.token.ViewModelFactory
 
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "token")
 class DashboardActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDashboardBinding
@@ -11,5 +20,14 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val pref = TokenPreference.getInstance(dataStore)
+        val tokenViewModel = ViewModelProvider(this, ViewModelFactory(pref))[TokenViewModel::class.java]
+
+        binding.logoutBtn.setOnClickListener {
+            tokenViewModel.saveToken("")
+            Thread.sleep(500)// Wait for token to be deleted
+            finish()
+        }
     }
 }
